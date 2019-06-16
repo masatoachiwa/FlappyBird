cbb9fafb147afd262cb124c1d2bbc2e6d6df267d
masato.achiwa <tokemdokea@yahoo.co.jp>
2019-06-16 18:57:42 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by アプリ開発 on 2019/06/16.
+//  Copyright © 2019 Masato.achiwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
