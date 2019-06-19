9df4eb0c8825afaa01cb0bfb420934c9c95b8779
masato.achiwa <tokemdokea@yahoo.co.jp>
2019-06-20 02:17:06 +0900

    背景青に変更

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by アプリ開発 on 2019/06/20.
+//  Copyright © 2019 Masato.achiwa. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+       backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+    }
+
+  
+
+        // Configure the view for the selected state
+    }
+
+
--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+        // SKViewに型を変換する SpriteKitのこと。ゲームを作るためのフレームワーク。
+        let skView = self.view as! SKView
+        
+        // FPSを表示する。画面が一秒間に何回更新されるかを示す。
+        skView.showsFPS = true
+        
+        // ノードの数を表示する。テキストやキャラクター。画像などの物体を表示します。
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)  //GameSceneクラスに変更
+        
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
+    
+    
+
     }
 
 
